35f8047fcba82bb44e35dd04bee6754ad8c51e13
dina hellal <dinahellal12@gmail.com>
2025-04-09 17:37:24 +0200

    Add: Header Section

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+document.getElementById("moon").onclick = () => {
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+    document.getElementById("moon").src = "assets/icons/9254147_moon_crescent_night_mode_dark_icon.png";
+  } else {
+    document.body.classList.add("dark-theme");
+    document.getElementById("moon").src = "assets/icons/430079_brightness_sun_icon.png";
+  }
+};
+
+  function goToPage() {
+    window.location.href = "page2.html"; 
+  }
